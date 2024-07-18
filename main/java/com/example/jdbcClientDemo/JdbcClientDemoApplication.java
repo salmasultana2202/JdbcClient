43e102815fb2fd9f61843a8ed7153ed5f2c26903
salmasultana2202 <salmasultana1102@gmail.com>
2024-07-18 20:19:28 +0530

    first commit

---
+++ main/java/com/example/jdbcClientDemo/JdbcClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbcClientDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcClientDemoApplication.class, args);
+	}
+
+}
---
+++ test/java/com/example/jdbcClientDemo/JdbcClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbcClientDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcClientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
